5c2d2eb5568f390f0e2d1a0311cfe1819eeebbcf
dinozoff1981 <tensaeg@yahoo.com>
2022-12-11 08:28:55 +0100

    try1

--- connect.php
+++ connect.php
@@ -5,14 +5,14 @@ $con=new mysqli("sql.freedb.tech","freedb_tensae","6Ssqn7ab#T%7PeH","freedb_shal
 
 
 if(!$con){
-    die('Connection Failed'. mysqli_connect_error($con));
-}
-
-
-$con=new mysqli("localhost","root","","shalomdb");
-
-if(!$con){
    die('Connection Failed'. mysqli_connect_error($con));
 }
+
+
+//$con=new mysqli("localhost","root","","shalomdb");
+
+//if(!$con){
+   //die('Connection Failed'. mysqli_connect_error($con));
+//}
 
 ?>
