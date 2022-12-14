93b8142f7e72f68583dd6b4799b6856d932b0016
dinozoff1981 <tensaeg@yahoo.com>
2022-12-14 13:27:56 +0100

    dbchange

--- connect.php
+++ connect.php
@@ -1,18 +1,26 @@
 <?php
 
 
-//$con=new mysqli("sql.freedb.tech","freedb_dinos","6Ssqn7ab#T%7PeH","freedb_shalomdb");
+//$con=new mysqli("sql.freedb.tech","freedb_dinos","FV6knd%e6B%p!@&","freedb_shalom");
 
 
 //if(!$con){
-  // die('Connection Failed'. mysqli_connect_error($con));
+  //die('Connection Failed'. mysqli_connect_error($con));
 //}
 
 
-$con=new mysqli("localhost","root","","shalomdb");
+$con=new mysqli("remotemysql.com","d62tD0p7cf","JMYHTSKunI","fd62tD0p7cf");
+
 
 if(!$con){
-   die('Connection Failed'. mysqli_connect_error($con));
+  die('Connection Failed'. mysqli_connect_error($con));
 }
+
+
+//$con=new mysqli("localhost","root","","shalomdb");
+
+//if(!$con){
+   //die('Connection Failed'. mysqli_connect_error($con));
+//}
 
 ?>
